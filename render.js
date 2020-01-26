ca098d4407fd4a848447edc3b3062179e7cc70d9
YamOhana <59656001+YamOhana@users.noreply.github.com>
2020-01-26 19:09:43 +0200

    Add files via upload
    rendered

---
+++ render.js
@@ -0,0 +1,23 @@
+const Renderer = function () {
+
+
+    const renderPosts = function (posts) {
+        $("#posts").empty()
+        let appendhtml = ""
+        for (const post of posts) {
+            appendhtml = ""
+            appendhtml += `<div class="post" data-id=${post.id}>${post.text}`
+            // $("#posts").append(`</div>`)
+            for (const comment of post.comments) {
+                // $(".post").append(`<div class="comments" data-id=${comment.id}>${comment.text}</div>`)
+                appendhtml += `<div class="comments" data-id=${comment.id}><span class="delete-comment">X</span>${comment.text}</div>`
+            }
+            appendhtml += `<div><input type="text" placeholder="Add a comment"><button class="newcomment">comment</button></div><button class="delete">Delete Post</button></div>`
+            $("#posts").append(appendhtml)
+        }
+    }
+    return {
+        renderPosts: renderPosts
+    }
+
+}
