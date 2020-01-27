c995e3b23529aa549edcc6c948bb4e8e03614590
YamOhana <yamoh1993@gmail.com>
2020-01-27 10:42:09 +0200

    Tweeter project 27/01
    Tweeter project updated 27.01 11:00 am

---
+++ render.js
@@ -0,0 +1,21 @@
+const Renderer = function () {
+
+
+    const renderPosts = function (posts) {
+        $("#posts").empty()
+        let appendhtml = ""
+        for (const post of posts) {
+            appendhtml = ""
+            appendhtml += `<div class="post" data-id=${post.id}>${post.text}`
+            for (const comment of post.comments) {
+                appendhtml += `<div class="comments" data-id=${comment.id}><span class="delete-comment">X</span>${comment.text}</div>`
+            }
+            appendhtml += `<div><input type="text" placeholder="Add a comment"><button class="newcomment">comment</button></div><button class="delete">Delete Post</button></div>`
+            $("#posts").append(appendhtml)
+        }
+    }
+    return {
+        renderPosts: renderPosts
+    }
+
+}
